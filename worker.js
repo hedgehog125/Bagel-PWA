c727227a5f262e988edd36e131fdd79f779b56d0
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-06-21 21:21:34 +0100

    Maybe this will fix it
    I thought I just finally discovered what the problem was but it's not that. Maybe just writing better code will fix it?

--- worker.js
+++ worker.js
@@ -1,1 +1,27 @@
-let index=location.href.split("/");index.pop();const toCache=[index=index.join("/")+"/",...["bagel.js","assets/imgs/bagel.png","assets/imgs/icons/128x128.png","assets/imgs/icons/144x144.png","assets/imgs/icons/152x152.png","assets/imgs/icons/192x192.png","assets/imgs/icons/256x256.png","assets/imgs/icons/512x512.png","manifest.json","worker.js"]];self.addEventListener("install",e=>{self.skipWaiting()});const useCache=e=>cache?e():caches.open("Bagel.js Bagel").then(s=>(cache=s,e()));let cache;self.addEventListener("fetch",e=>{e.respondWith(useCache(s=>cache.match(e.request).then(s=>{if(s)return s;{let s=fetch(e.request);return s.then(s=>{let t=e.request.url,n=t.replace(index,"");(t==index||toCache.includes(n))&&cache.put(e.request,s.clone())}),s.catch(s=>{console.warn("A Bagel.js service worker failed to fetch "+e.request.url+". Request:"),console.log({...e.request})}),s}})))});
+let index = location.href.split("/");
+index.pop();
+const toCache = [index = index.join("/") + "/", "bagel.js", "assets/imgs/bagel.png", "assets/imgs/icons/128x128.png", "assets/imgs/icons/144x144.png", "assets/imgs/icons/152x152.png", "assets/imgs/icons/192x192.png", "assets/imgs/icons/256x256.png", "assets/imgs/icons/512x512.png", "manifest.json", "worker.js"];
+self.addEventListener("install", e=>{
+    self.skipWaiting()
+}
+);
+self.addEventListener("fetch", async e => {
+    let cache = await caches.open("Bagel.js Bagel");
+    let cached = await cache.match(e.request);
+    if (cached) return cached;
+
+    let resource;
+    try {
+        resource = await fetch(e.request);
+    }
+    catch (error) {
+        console.warn("A Bagel.js service worker failed to fetch " + e.request.url + ". Request:");
+        console.log({
+            ...e.request
+        });
+    }
+    if (toCache.includes(e.request.url)) {
+        cache.put(e.request, resource.clone());
+    }
+    return resource;
+});
