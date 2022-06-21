33af7881610e47df8dcd7d33af490c591ad36bb7
hedgehog125 <16543008+hedgehog125@users.noreply.github.com>
2022-06-21 22:12:40 +0100

    oops
    I should probably try and test this locally

--- worker.js
+++ worker.js
@@ -1,6 +1,6 @@
 let index = location.href.split("/");
 index.pop();
-const toCache = [index = index.join("/") + "/", ["bagel.js", "assets/imgs/bagel.png", "assets/imgs/icons/128x128.png", "assets/imgs/icons/144x144.png", "assets/imgs/icons/152x152.png", "assets/imgs/icons/192x192.png", "assets/imgs/icons/256x256.png", "assets/imgs/icons/512x512.png", "manifest.json", "worker.js"].map(href => index + href)];
+const toCache = [index = index.join("/") + "/", ...["bagel.js", "assets/imgs/bagel.png", "assets/imgs/icons/128x128.png", "assets/imgs/icons/144x144.png", "assets/imgs/icons/152x152.png", "assets/imgs/icons/192x192.png", "assets/imgs/icons/256x256.png", "assets/imgs/icons/512x512.png", "manifest.json", "worker.js"].map(href => index + href)];
 self.addEventListener("install", e=>{
     self.skipWaiting()
 }
